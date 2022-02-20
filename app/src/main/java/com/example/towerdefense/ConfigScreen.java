dcb6cf2f9886f671e52cabf9674ff9523eb1efb0
Pujith Veeravelli <pujithvi@gmail.com>
2022-02-19 20:46:51 -0500

    initial commit with working welcome and config screens

---
+++ app/src/main/java/com/example/towerdefense/ConfigScreen.java
@@ -0,0 +1,65 @@
+package com.example.towerdefense;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConfigScreen extends AppCompatActivity {
+    private Spinner difficulty;
+    private Button submit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+        setContentView(R.layout.activity_config_screen);
+
+        submit = (Button) findViewById(R.id.submitButton);
+        difficulty = (Spinner) findViewById(R.id.difficultyInput);
+        List<String> spinnerArray = new ArrayList<>();
+        spinnerArray.add("Easy");
+        spinnerArray.add("Medium");
+        spinnerArray.add("Hard");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, spinnerArray);
+        difficulty.setAdapter(adapter);
+
+        EditText text = (EditText)findViewById(R.id.nameInput);
+
+        TextView warning = (TextView)findViewById(R.id.warningMessage);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String selected = difficulty.getSelectedItem().toString();
+                String nameInputted = text.getText().toString();
+                if (nameInputted != null && !nameInputted.trim().isEmpty()){
+                    openGameScreen();
+                } else {
+                    warning.setText("Please Enter a Valid Name!");
+                }
+            }
+        });
+    }
+
+    public void openGameScreen(){
+        Intent intent = new Intent(this, GameScreen.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/towerdefense/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.towerdefense;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+    private Button start;
+    private Button quit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+        setContentView(R.layout.activity_main);
+
+        start = (Button) findViewById(R.id.button2);
+        start.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                openConfigScreen();
+            }
+        });
+        quit = (Button) findViewById(R.id.button);
+        quit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                System.exit(0);
+            }
+        });
+    }
+
+    public void openConfigScreen() {
+        Intent intent = new Intent(this, ConfigScreen.class);
+        startActivity(intent);
+    }
+}
