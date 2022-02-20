3fe403872d26c0480324c5059f8a02ffed25a88b
sripushkar <sripushkar@gmail.com>
2022-02-20 01:28:59 -0500

    create Monument class and constructor

---
+++ app/src/main/java/com/example/towerdefense/Monument.java
@@ -0,0 +1,20 @@
+package com.example.towerdefense;
+
+public class Monument {
+    private double health;
+    private double money;
+    //add any other attributes here. make sure to update constructors
+    // and write getters/setters when you do that
+
+    //default constructor
+    public Monument() {
+
+    }
+
+    public Monument(double health, double money) {
+        this.health = health;
+        this.money = money;
+    }
+
+
+}
