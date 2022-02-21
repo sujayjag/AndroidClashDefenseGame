ef16d2a731252a875b26aa4a0946d2ceefb5d6e8
Pujith Veeravelli <pujithvi@gmail.com>
2022-02-20 22:42:47 -0500

    Change scaling of images

--- app/src/main/java/com/example/towerdefense/HardScreen.java
+++ app/src/main/java/com/example/towerdefense/HardScreen.java
@@ -2,6 +2,7 @@ package com.example.towerdefense;
 
 import android.os.Bundle;
 import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +15,10 @@ public class HardScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
         setContentView(R.layout.activity_hard_screen);
         money = findViewById(R.id.money3);
         health = findViewById(R.id.health3);
