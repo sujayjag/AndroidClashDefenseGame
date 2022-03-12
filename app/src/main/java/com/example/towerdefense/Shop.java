6ef2c9fad4f5797f1df2b6a8dcab7fbd9e70b499
sripushkar <sripushkar@gmail.com>
2022-03-12 17:09:53 -0500

    create cannon classes

--- app/src/main/java/com/example/towerdefense/Shop.java
+++ app/src/main/java/com/example/towerdefense/Shop.java
@@ -3,7 +3,7 @@ package com.example.towerdefense;
 public class Shop {
     public boolean buyTower(Tower tower, Player player) {
         if (player.balance >= tower.cost) {
-            player.balance -= tower.cost;
+            player.updateBalance(-1*tower.cost);
             return true;
         } else {
             return false;
