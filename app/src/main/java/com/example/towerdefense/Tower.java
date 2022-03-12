8621acf94c5ac4d58d893ae204d382e5aae82f92
sripushkar <sripushkar@gmail.com>
2022-03-12 17:16:30 -0500

    fix some typos

--- app/src/main/java/com/example/towerdefense/Tower.java
+++ app/src/main/java/com/example/towerdefense/Tower.java
@@ -1,27 +1,29 @@
 package com.example.towerdefense;
 
-public abstract class Tower {
-    public int cost;
-    public int level;
-    public double upgradeMultiplier;
-    public int attackSpeed;
-    public int attackDamage;
-    public String location;
-    public String imageString;
+import androidx.appcompat.app.AppCompatActivity;
 
-    public Tower(int cost, int level, double upgradeMultiplier, int attackSpeed, int attackDamage, String location, String imageString) {
-        this.cost = cost;
-        this.level = level;
-        this.upgradeMultiplier = upgradeMultiplier;
-        this.attackSpeed = attackSpeed;
-        this.attackDamage = attackDamage;
-        this.location = location;
-        this.imageString = imageString;
-    }
+public abstract class Tower extends AppCompatActivity {
+    public int cost = 100;
+    public int level = 1;
+    public double upgradeMultiplier = 1;
+    public int attackSpeed = 1;
+    public int attackDamage = 20;
+    public String location = "";
+    public String imageString = "";
 
-    public Tower(){
-        this(100, 1, 1, 20, 1, "", "");
-    }
+//    public Tower(int cost, int level, double upgradeMultiplier, int attackSpeed, int attackDamage, String location, String imageString) {
+//        this.cost = cost;
+//        this.level = level;
+//        this.upgradeMultiplier = upgradeMultiplier;
+//        this.attackSpeed = attackSpeed;
+//        this.attackDamage = attackDamage;
+//        this.location = location;
+//        this.imageString = imageString;
+//    }
+//
+//    public Tower(){
+//        this(100, 1, 1, 20, 1, "", "");
+//    }
 
     //abstract public void upgrade();
 }
